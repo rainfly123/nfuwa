7927e56a4ccd963946e049eeb06f7bb637379674
Your Name <you@example.com>
2017-05-30 11:13:36 +0800

    class

--- redi.py
+++ redi.py
@@ -276,8 +276,8 @@ def Huodong(gid):
         detail = ""
     return detail
 
-aclass =[{"name":"美食", "enum":1}, {"name":"女装", "enum":2}, {"name":"男装", "enum":3},\
-        {"name":"鞋帽", "enum":4}, {"name":"玩乐", "enum":5}]
+aclass =[{"name":"美食", "enum":"1"}, {"name":"女装", "enum":"2"}, {"name":"男装", "enum":"3"},\
+        {"name":"鞋帽", "enum":"4"}, {"name":"玩乐", "enum":"5"}]
 def Class():
     return aclass
 
