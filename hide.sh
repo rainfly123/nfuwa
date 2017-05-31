3528ffd432608e132da344505780ec4e70d2c26c
rainfly <xiechc@gmail.com>
2017-05-31 11:43:34 +0800

    hide

--- hide.sh
+++ hide.sh
@@ -1,4 +1,4 @@
 #!/bin/sh 
 #curl -F "file=@${1}" http://live.66boss.com/upload/writev2
-curl -F "file=@${1}" -F "video=@${2}" "http://fuwa2.66boss.com:9090/api/hide?owner=100000076&detail=ddd&fuwagid=fuwa_c_100&pos=dd&geohash=102.23-33.22"
+curl -F "file=@${1}" -F "video=@${2}" "http://fuwa.hmg66.com/api/hidev2?owner=100000320&detail=店内活动&pos=沙县小吃&geohash=113.3-23.08&validtime=1&number=5&type=1&class=1"
 
