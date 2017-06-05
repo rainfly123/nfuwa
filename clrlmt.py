5e0490370954067941f56d20eee54d2392081706
Your Name <you@example.com>
2017-06-05 10:49:06 +0800

    clear limitation

--- clrlmt.py
+++ clrlmt.py
@@ -3,6 +3,7 @@
 import redis
 
 r = redis.Redis(host='127.0.0.1', port=6379, db=1, password="aaa11bbb22")
+s=100000000
 for x in xrange(10000):
-    key = str(x) + "_lmt"
+    key = str(s + x) + "_lmt"
     r.delete(key)
