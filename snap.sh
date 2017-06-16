6347e1fa15d578453b7694b5932c76b676a0d298
rainfly <xiechc@gmail.com>
2017-06-16 11:32:52 +0800

    ...

--- snap.sh
+++ snap.sh
@@ -8,18 +8,18 @@ fi
 
 touch $pid
 
-    files=$(ls *.mp4 -t | head -30)
-    for file in $files
-        do
-        jpg=${file%.*}.jpg
-        if [ ! -f $jpg ]
-        then
-            /usr/local/bin/ffmpeg -loglevel quiet -i $file -vframes 1 -f image2 -y $jpg
-          #  nohup qrsctl put -c wsim fuwa/$file   $file &
-            ./tool.py $jpg
-        fi
-    done
-    sleep 1
+#    files=$(ls *.mp4 -t | head -30)
+#    for file in $files
+#        do
+#        jpg=${file%.*}.jpg
+#        if [ ! -f $jpg ]
+#        then
+#            /usr/local/bin/ffmpeg -loglevel quiet -i $file -vframes 1 -f image2 -y $jpg
+#          #  nohup qrsctl put -c wsim fuwa/$file   $file &
+#            ./tool.py $jpg
+#        fi
+#    done
+#    sleep 1
     files=$(ls *.mov -t | head -30)
     for file in $files
         do
